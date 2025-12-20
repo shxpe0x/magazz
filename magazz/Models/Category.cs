0cbf5c6b97b5d10b00199a7cf8c65be66f03a78b
shxpe <qwezxasd1@yandex.ru>
2025-12-20 11:06:00 +0400

    Add Category model

---
+++ magazz/Models/Category.cs
@@ -0,0 +1,12 @@
+namespace magazz.Models
+{
+    public class Category
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public string? Description { get; set; }
+        
+        // Navigation property
+        public ICollection<Product> Products { get; set; } = new List<Product>();
+    }
+}
