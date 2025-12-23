0ab9f3ec3656b3fa48fb00976b23f865c8cafc88
shxpe <qwezxasd1@yandex.ru>
2025-12-23 17:46:18 +0400

    Configure ASP.NET Core Identity services in Program.cs

--- magazz/Program.cs
+++ magazz/Program.cs
@@ -1,5 +1,7 @@
 using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Identity;
 using magazz.Data;
+using magazz.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -9,6 +11,41 @@ builder.Services.AddControllersWithViews();
 // Добавляем Entity Framework и SQLite
 builder.Services.AddDbContext<AppDbContext>(options =>
     options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+// Настройка ASP.NET Core Identity
+builder.Services.AddIdentity<ApplicationUser, IdentityRole>(options =>
+{
+    // Настройки пароля
+    options.Password.RequireDigit = true;
+    options.Password.RequireLowercase = true;
+    options.Password.RequireUppercase = true;
+    options.Password.RequireNonAlphanumeric = false;
+    options.Password.RequiredLength = 6;
+    
+    // Настройки блокировки
+    options.Lockout.DefaultLockoutTimeSpan = TimeSpan.FromMinutes(5);
+    options.Lockout.MaxFailedAccessAttempts = 5;
+    options.Lockout.AllowedForNewUsers = true;
+    
+    // Настройки пользователя
+    options.User.RequireUniqueEmail = true;
+    
+    // Настройки входа
+    options.SignIn.RequireConfirmedEmail = false;
+    options.SignIn.RequireConfirmedAccount = false;
+})
+.AddEntityFrameworkStores<AppDbContext>()
+.AddDefaultTokenProviders();
+
+// Настройка cookie аутентификации
+builder.Services.ConfigureApplicationCookie(options =>
+{
+    options.LoginPath = "/Account/Login";
+    options.LogoutPath = "/Account/Logout";
+    options.AccessDeniedPath = "/Account/AccessDenied";
+    options.ExpireTimeSpan = TimeSpan.FromDays(30);
+    options.SlidingExpiration = true;
+});
 
 var app = builder.Build();
 
@@ -29,6 +66,8 @@ if (!app.Environment.IsDevelopment())
 app.UseHttpsRedirection();
 app.UseRouting();
 
+// ВАЖНО: Authentication должен быть ДО Authorization
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapStaticAssets();
